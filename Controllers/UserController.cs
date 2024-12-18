6d3db1072ec531c0e91d79beb8b33f97f067b587
github-cloudlabsuser-1282 <168007320+github-cloudlabsuser-1282@users.noreply.github.com>
2024-12-18 07:08:47 +0000

    crud

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,65 +2,120 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MyMvcApp.Models;
 
-namespace MyMvcApp.Controllers;
-
-public class UserController : Controller
+namespace MyMvcApp.Controllers
 {
-    public static System.Collections.Generic.List<User> userlist = new System.Collections.Generic.List<User>();
+    public class UserController : Controller
+    {
+        // Simulating a database with an in-memory list of users
+        public static System.Collections.Generic.List<User> userlist = new System.Collections.Generic.List<User>();
 
         // GET: User
         public ActionResult Index()
         {
-            // Implement the Index method here
+            // Display the list of all users
+            return View(userlist);
         }
 
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
-            // Implement the details method here
+            // Find the user with the given ID
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound(); // Return 404 if user not found
+            }
+
+            return View(user);
         }
 
         // GET: User/Create
         public ActionResult Create()
         {
-            //Implement the Create method here
+            // Display the form for creating a new user
+            return View();
         }
 
         // POST: User/Create
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(User user)
         {
-            // Implement the Create method (POST) here
+            if (ModelState.IsValid)
+            {
+                // Add the new user to the list
+                user.Id = userlist.Count > 0 ? userlist.Max(u => u.Id) + 1 : 1; // Assign a unique ID
+                userlist.Add(user);
+                return RedirectToAction(nameof(Index));
+            }
+
+            // If validation fails, show the form again
+            return View(user);
         }
 
         // GET: User/Edit/5
         public ActionResult Edit(int id)
         {
-            // This method is responsible for displaying the view to edit an existing user with the specified ID.
-            // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+            // Find the user with the given ID
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound(); // Return 404 if user not found
+            }
+
+            return View(user);
         }
 
         // POST: User/Edit/5
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Edit(int id, User user)
         {
-            // This method is responsible for handling the HTTP POST request to update an existing user with the specified ID.
-            // It receives user input from the form submission and updates the corresponding user's information in the userlist.
-            // If successful, it redirects to the Index action to display the updated list of users.
-            // If no user is found with the provided ID, it returns a HttpNotFoundResult.
-            // If an error occurs during the process, it returns the Edit view to display any validation errors.
+            if (ModelState.IsValid)
+            {
+                // Find the existing user and update their details
+                var existingUser = userlist.FirstOrDefault(u => u.Id == id);
+                if (existingUser == null)
+                {
+                    return NotFound(); // Return 404 if user not found
+                }
+
+                existingUser.Name = user.Name;
+                existingUser.Email = user.Email;
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            // If validation fails, show the form again
+            return View(user);
         }
 
         // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
-            // Implement the Delete method here
+            // Find the user with the given ID
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound(); // Return 404 if user not found
+            }
+
+            return View(user);
         }
 
         // POST: User/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, IFormCollection collection)
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
         {
-            // Implement the Delete method (POST) here
+            // Find the user and remove them from the list
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user != null)
+            {
+                userlist.Remove(user);
+            }
+
+            return RedirectToAction(nameof(Index));
         }
+    }
 }
